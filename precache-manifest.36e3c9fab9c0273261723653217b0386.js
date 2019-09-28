efe6c534e6ef6406397b0295df8c137359803daf
ugl5 <iok97531@gmail.com>
2019-09-28 15:58:57 +0900

    Updates

---
+++ precache-manifest.36e3c9fab9c0273261723653217b0386.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa539586292e92d911537d24fe0f636",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2c60eda1fc9f646b23c2",
+    "url": "/movie_app/static/css/main.9f4069f4.chunk.css"
+  },
+  {
+    "revision": "f5e14d3c5eb6080e03ff",
+    "url": "/movie_app/static/js/2.deda57b5.chunk.js"
+  },
+  {
+    "revision": "2c60eda1fc9f646b23c2",
+    "url": "/movie_app/static/js/main.e346727f.chunk.js"
+  },
+  {
+    "revision": "d179891a5f5e4ba41521",
+    "url": "/movie_app/static/js/runtime~main.76fa3daa.js"
+  }
+]);
